b9e3867345241c2c1832cd4c5bb3ae9ddf3c75ca
Deep Siroya <53616046+deepsiroya17@users.noreply.github.com>
2021-10-13 00:52:19 +0530

    Folders Created

--- App.js
+++ App.js
@@ -1,24 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-import LoginView from "./src/accountlogin/Login";
-import SignUpView from "./src/accountlogin/SignUp";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const SignUpStack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <SignUpStack.Navigator initialRouteName="SignUpView">
-        <SignUpStack.Screen name="SignUpView" component={SignUpView} />
-        <SignUpStack.Screen name="LoginView" component={LoginView} />
-      </SignUpStack.Navigator>
-    </NavigationContainer>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
