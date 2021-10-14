5f79144a0fcb962739f726bc589200e5d1db2689
Deep Siroya <53616046+deepsiroya17@users.noreply.github.com>
2021-10-14 16:37:15 +0530

    Added Login, Drawer & Tab

---
+++ src/screens/MainTabScreen.js
@@ -0,0 +1,107 @@
+import React from 'react';
+
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import HomeScreen from './HomeScreen';
+import DetailsScreen from './DetailsScreen';
+import ExploreScreen from './ExploreScreen';
+import ProfileScreen from './ProfileScreen';
+
+const HomeStack = createNativeStackNavigator();
+const DetailsStack = createNativeStackNavigator();
+
+const Tab = createMaterialBottomTabNavigator();
+
+const MainTabScreen = () => (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#fff"
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeStackScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarColor: '#009387',
+          tabBarIcon: () => (
+            <Icon name="ios-home" color="#fff" size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={DetailsStackScreen}
+        options={{
+          tabBarLabel: 'Updates',
+          tabBarColor: '#1f65ff',
+          tabBarIcon: () => (
+            <Icon name="ios-notifications" color="#fff" size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarColor: '#694fad',
+          tabBarIcon: () => (
+            <Icon name="ios-person" color="#fff" size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Explore"
+        component={ExploreScreen}
+        options={{
+          tabBarLabel: 'Explore',
+          tabBarColor: '#d02860',
+          tabBarIcon: () => (
+            <Icon name="ios-aperture" color="#fff" size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+);
+
+export default MainTabScreen;
+
+const HomeStackScreen = ({navigation}) => (
+<HomeStack.Navigator screenOptions={{
+        headerStyle: {
+        backgroundColor: '#009387',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+        fontWeight: 'bold'
+      }
+    }}>
+        <HomeStack.Screen name="MainTab" component={HomeScreen} options={{
+        title:'Overview',
+        headerLeft: () => (
+            <Icon.Button name="ios-menu" size={25} backgroundColor="#009387" onPress={() => navigation.openDrawer()}></Icon.Button>
+        )
+        }} />
+</HomeStack.Navigator>
+);
+
+const DetailsStackScreen = ({navigation}) => (
+<DetailsStack.Navigator screenOptions={{
+        headerStyle: {
+        backgroundColor: '#1f65ff',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+        fontWeight: 'bold'
+        }
+    }}>
+        <DetailsStack.Screen name="Details" component={DetailsScreen} options={{
+        headerLeft: () => (
+            <Icon.Button name="ios-menu" size={25} backgroundColor="#1f65ff" onPress={() => navigation.openDrawer()}></Icon.Button>
+        )
+        }} />
+</DetailsStack.Navigator>
+);
